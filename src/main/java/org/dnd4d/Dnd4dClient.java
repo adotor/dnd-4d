43593c5fb57c79f751bc918a6141cc2ec6fbb434
Dotor, Dr. Alexander <Alexander.Dotor@senacor.com>
2017-09-21 17:09:59 +0200

    Added Dnd4dClient with tests.

--- src/main/java/org/dnd4d/Dnd4dClient.java
+++ src/main/java/org/dnd4d/Dnd4dClient.java
@@ -1,10 +1,27 @@
 package org.dnd4d;
 
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.WebSocketFrame;
 
 public class Dnd4dClient extends AbstractVerticle {
+
+    private enum State {
+        ON,OFF
+    }
+
+    private LedController ledController;
+
+    public Dnd4dClient() {
+        this.ledController = new LedController(RaspiPin.GPIO_01, PinState.LOW);
+    }
+
+    public Dnd4dClient(LedController ledController) {
+        this.ledController = ledController;
+    }
 
     @Override
     public void start(Future<Void> fut) {
@@ -13,9 +30,39 @@ public class Dnd4dClient extends AbstractVerticle {
                 .websocket("/myuri", websocket -> {
                     websocket.frameHandler(frame -> {
                         System.out.println("Client: Received message:" + frame.textData());
+
+                        if (!canHandle(frame)) {
+                            System.out.println("Ignoring unknown event.");
+                        }
+
+                        final State state = State.valueOf(frame.textData());
+                        switch (state) {
+                            case ON:
+                                ledController.on();
+                                break;
+                            case OFF:
+                                ledController.off();
+                                break;
+                        }
+                    })
+                    .closeHandler(unused -> {
                         fut.complete();
                     });
                 });
 
+    }
+
+    private boolean canHandle(WebSocketFrame frame) {
+        if (!frame.isText()) {
+            return false;
+        }
+
+        try {
+            State.valueOf(frame.textData());
+        } catch (IllegalArgumentException ex) {
+            return false;
+        }
+
+        return true;
     }
 }
--- src/test/java/org/dnd4d/Dnd4dClientTest.java
+++ src/test/java/org/dnd4d/Dnd4dClientTest.java
@@ -10,17 +10,38 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Optional.ofNullable;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 @RunWith(VertxUnitRunner.class)
 public class Dnd4dClientTest {
 
     private Vertx vertx;
 
+    private Dnd4dClient sut;
+
     private HttpServer testServer;
+
+    @Mock
+    private LedController ledController;
 
     @Before
     public void setUp() {
-        testServer = createTestServer();
+        initMocks(this);
+
+        sut = new Dnd4dClient(ledController);
     }
 
     @After
@@ -28,23 +49,80 @@ public class Dnd4dClientTest {
         testServer.close();
     }
 
-    private HttpServer createTestServer() {
-        return Vertx.vertx().createHttpServer();
+    private HttpServer createTestServer(int port, List<String> messages, Optional<Async> async) {
+        return Vertx.vertx().createHttpServer()
+                .websocketHandler(websocket -> {
+                    messages.stream().forEach(websocket::writeFinalTextFrame);
+                    websocket.close();
+                    async.ifPresent(Async::complete);;
+                })
+                .listen(port);
+
     }
 
     @Test(timeout = 1000)
     public void start_clientConnectsToServer(TestContext context) {
         Async async = context.async();
 
-        testServer.websocketHandler(websocket -> {
-            System.out.println("Server: Client connected!");
-            websocket.writeFinalTextFrame("Hallo");
-            async.complete();
-        })
-        .listen(8081);
+        testServer = createTestServer(8081, emptyList(), ofNullable(async));
 
         vertx = Vertx.vertx();
-        vertx.deployVerticle(Dnd4dClient.class.getName(),
+        vertx.deployVerticle(sut,
                 context.asyncAssertSuccess());
     }
-}
+
+    @Test(timeout = 1000)
+    public void start_clientTurnsOnLed(TestContext context) {
+        Async async = context.async();
+
+        testServer = createTestServer(8081, asList("ON"), ofNullable(async));
+
+        vertx = Vertx.vertx();
+        vertx.deployVerticle(sut,
+                context.asyncAssertSuccess(result -> {
+                    verify(ledController).on();
+                    verify(ledController, never()).off();
+                }));
+    }
+
+    @Test(timeout = 1000)
+    public void start_clientTurnsOffLed(TestContext context) {
+        Async async = context.async();
+
+        testServer = createTestServer(8081, asList("OFF"), ofNullable(async));
+
+        vertx = Vertx.vertx();
+        vertx.deployVerticle(sut,
+                context.asyncAssertSuccess(result -> {
+                    verify(ledController).off();
+                    verify(ledController, never()).on();
+                }));
+    }
+
+    @Test(timeout = 1000)
+    public void start_clientTurnsLedOnAndOffMutipleTimes(TestContext context) {
+        Async async = context.async();
+
+        testServer = createTestServer(8081, asList("ON", "OFF", "ON", "OFF"), ofNullable(async));
+
+        vertx = Vertx.vertx();
+        vertx.deployVerticle(sut,
+                context.asyncAssertSuccess(result -> {
+                    verify(ledController, times(2)).on();
+                    verify(ledController, times(2)).off();
+                }));
+    }
+
+    @Test(timeout = 1000)
+    public void start_clientIgnoresUnknownMessages(TestContext context) {
+        Async async = context.async();
+
+        testServer = createTestServer(8081, asList("Hallo"), ofNullable(async));
+
+        vertx = Vertx.vertx();
+        vertx.deployVerticle(sut,
+                context.asyncAssertSuccess(result -> {
+                    verify(ledController, never()).on();
+                    verify(ledController, never()).off();
+                }));
+    }}
